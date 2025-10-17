44e5b8a15c1281c4a367bc506ad71d92226e9354
alastrantia <niklas.lulay@icloud.com>
2025-10-17 15:14:34 +0200

    another state, now somewhat workds

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 # main.py
 # entrypoint for the CLI
-import sys, config, task
+import sys, config
 import saving
-arguments = sys.argv[1:]
-task_data = [task.Task("blah", "duh", "bla", "whocares", id="1"), task.Task("blah", "duh", "bla", "whocares", id="1")] # connected to json storage
-saving.save_json(task_data)
 
+arguments = sys.argv[1:]
+task_data = saving.load_json()
+print(task_data)
 
 # get the available commands from config.py
 commands = config.commands
@@ -18,6 +18,7 @@ def call_function(function_name, command_data, arguments):
     arguments.insert(0, task_data)
     print(f"calling {function_name}, {arguments}")
     task_data = command_data["function_call_name"](*arguments) # wow thanks unpacking operator
+    saving.save_json(task_data)
 
 
 if arguments == []:
--- src/saving.py
+++ src/saving.py
@@ -1,4 +1,5 @@
 import json
+import task
 
 def save_json(tasks, path="tasks.json"):
     json_text = "["
@@ -11,11 +12,21 @@ def save_json(tasks, path="tasks.json"):
             "updatedAt": tasks[i].updatedAt
         }
         if i == (len(tasks) - 1):
-             json_text += f"{json.dumps(current_task)}\n"
+             json_text += f"{json.dumps(current_task)}"
         else:
              json_text += f"{json.dumps(current_task)},\n"
     json_text += "]"
+    with open(path, "w") as f:
+        f.write(json_text)
     return json_text
 
-def load_json(path):
-    pass
+def load_json(path="tasks.json"):
+    new_data = []
+    with open(path, "r") as f:
+       contents = f.read()
+       if contents == "":
+           return []
+       json_data = json.loads(contents)
+    for item in json_data:
+       new_data.append(task.Task(item["description"], item["status"], item["createdAt"], item["updatedAt"], id=item["id"]))
+    return new_data
